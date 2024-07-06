4844c02624218eeea6797a8eea3a5986a2009bf7
fahnestockj <jrfz31@gmail.com>
2024-07-06 14:41:46 -0400

    feat: textnode class

---
+++ src/textnode.py
@@ -0,0 +1,12 @@
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, textnode):
+        return (self.text == textnode.text) and (self.text_type == textnode.text_type) and (self.url == textnode.url)
+
+    def __repre__(self):
+        return "TextNode("+self.text+","+self.text_type+","+self.url+")"
+
