ae4987feadcf8836e14ce654e80dc2e67a537e0d
fahnestockj <jrfz31@gmail.com>
2024-07-10 17:48:53 -0400

    feat: static build done

--- src/build.py
+++ src/build.py
@@ -1,28 +1,30 @@
 import os
 import shutil
 
-def build_static_file(rel_file_path):
-    src_path = os.path.abspath(rel_file_path)
+def build_static_file(abs_file_path):
+    src_path = abs_file_path
     abs_public_path = os.path.abspath("public")
     abs_static_path = os.path.abspath("static")
     relative_path_from_static = os.path.relpath(src_path, abs_static_path)
     dst_path = os.path.join(abs_public_path, relative_path_from_static)
-    par_path = os.path.join(dst_path, os.pardir)
+    par_path = os.path.abspath(os.path.join(dst_path, os.pardir))
     
-    
-    os.makedirs(par_path, True)
+    os.makedirs(par_path,0o777, True)
     shutil.copy(src_path, dst_path)
+
 
 
 def build_static_content():
     # clear out public dir
     shutil.rmtree("public")
-    walk_dir("static", build_static_file)
+    os.makedirs("public")
+    walk_dir(os.path.abspath("static"), build_static_file)
 
-def walk_dir(dir_relative_path: str, cb):
-    for entry in os.listdir(dir_relative_path):
-        if os.path.isdir(entry):
-            walk_dir(entry, cb)
+def walk_dir(dir_abs_path: str, cb):
+    for entry in os.listdir(dir_abs_path):
+        entry_path = os.path.join(dir_abs_path, entry)
+        if os.path.isdir(entry_path):
+            walk_dir(entry_path, cb)
         else:
-            cb(entry)
+            cb(entry_path)
 
